c55bde4bd6ea708c08e5c59dd15a5cba40291d9e
Rob Hawkes <rob.hawkes@gmail.com>
2011-06-04 18:05:42 +0100

    Fixed asynchronous issue

--- server.js
+++ server.js
@@ -6,13 +6,9 @@ var Game = require(__dirname+"/public/js/game/kaboom.game").KaboomGame;
 var Player = require(__dirname+"/public/js/game/kaboom.player").KaboomPlayer;
 
 var runningGame;
-fs.readFile("data/level.txt", "binary", function(err, file) {
- 	runningGame = new Game(file);
-	console.log(runningGame);
-
-	socket = io.listen(server);
-	setSocketHandlers();
-});
+var file = fs.readFileSync("data/level.txt", "binary");
+runningGame = new Game(file);
+console.log(runningGame);
 
 try {
   var configJSON = fs.readFileSync(__dirname + '/server/config.json');
@@ -42,6 +38,9 @@ server.get('/level', function(request, response) {
 server.listen(config.gameServer.port, config.gameServer.host);
 
 console.log("Kaboom! web server running on " + config.gameServer.host + ":" + config.gameServer.port);
+
+socket = io.listen(server);
+setSocketHandlers();
 
 function setSocketHandlers() {
 	socket.on("connection", function(client) {
