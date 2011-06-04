b9a139aa0ccbb945a7da661c797c8567b9dd46ad
Tom Parker <palfrey@tevp.net>
2011-06-04 10:55:26 +0100

    Output level on /level request

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require("fs");
  
 var server = express.createServer(express.logger());
 
 server.get('/', function(request, response) {
-  response.send("There's supposed to be an earth-shattering kaboom...\n");
+	response.send("There's supposed to be an earth-shattering kaboom...\n");
+});
+
+server.get('/level', function(request, response) {
+	fs.readFile("level.txt", "binary", function(err, file)
+		{
+			response.send({"level":file});
+		}
+	);
 });
 
 server.listen(process.env.PORT || 80);
