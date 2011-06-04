f4bc447f24b0f7dbb2fc290252e3af669402a81f
Dylan Beattie <dylan@dylanbeattie.net>
2011-06-04 10:51:13 +0100

    Added console.log to server startup code

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ var server = express.createServer(express.logger());
 server.get('/', function(request, response) {
   response.send("There's supposed to be an earth-shattering kaboom...\n");
 });
+
 server.listen(config.gameServer.port, config.gameServer.host);
+
+console.log("Kaboom! web server running on " + config.gameServer.host + ":" + config.gameServer.port);
