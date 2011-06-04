9c04bd3cb565ffabf6356fe8d4ade4274ad3eb2e
Rob Hawkes <rob.hawkes@gmail.com>
2011-06-04 11:16:00 +0100

    - Added initial message sending and parsing

--- client.js
+++ client.js
@@ -1,28 +1,48 @@
-var socket = new io.Socket("localhost", {port: 5678, transports: ["websocket", "flashsocket"]});
-
-// EVENTS
-// connect, connecting, connect_failed, message, close,
-// disconnect, reconnect, reconnecting, reconnect_failed
-
-// WebSocket connection successful
-socket.on("connect", function() {
-	console.log("Connected");
+var socket,
+	levelData;
 	
+$("#join").click(function() {
+	initSocket();
 });
-
-// WebSocket connection failed
-socket.on("connect_failed", function() {
-	console.log("Connect failed");
 	
-});
+var initSocket = function() {
+	socket = new io.Socket("localhost", {port: 5678, transports: ["websocket", "flashsocket"]});
 
-// WebSocket disconnection
-socket.on("disconnect", function() {
-	console.log("Disconnected");
-	
-});
+	// EVENTS
+	// connect, connecting, connect_failed, message, close,
+	// disconnect, reconnect, reconnecting, reconnect_failed
 
-// WebSocket message received
-socket.on("message", function(data) {
+	// WebSocket connection successful
+	socket.on("connect", function() {
+		console.log("Connected");
+		var msg = JSON.stringify({type: "join"});
+		socket.send(msg);
+	});
+
+	// WebSocket connection failed
+	socket.on("connect_failed", function() {
+		console.log("Connect failed");
+
+	});
+
+	// WebSocket disconnection
+	socket.on("disconnect", function() {
+		console.log("Disconnected");
+
+	});
+
+	// WebSocket message received
+	socket.on("message", function(data) {
+		var msg = .parse(data);
+		
+		if (msg.type) {
+			switch (msg.type) {
+				case "welcome":
+					levelData = msg.level;
+					break;
+			};
+		};
+	});
 	
-});
+	socket.connect();
+};
